1c0c28de6b8a51e9b6a86ad5ec1a3e9c02b516e7
Xolani <mazi76erx@gmail.com>
2024-05-31 13:11:02 +0000

    Added models for app and needed env files

--- server/models.py
+++ server/models.py
@@ -2,8 +2,8 @@
 
 from sqlalchemy import Column, DateTime, func
 
-# from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
-# from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
 Base: DeclarativeMeta = declarative_base()
@@ -16,18 +16,53 @@ class TimestampMixin:
     updated_at = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
+    
+class User(Base, TimestampMixin):
+    """Model for a user."""
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
 
-# Example model definitions
-# class Item(Base, TimestampMixin):
-#     """Model for an item in the inventory."""
+class Song(Base, TimestampMixin):
+    """Model for a song to convert to a playlist."""
 
-#     __tablename__ = "items"
+    __tablename__ = "songs"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, max_length=100, nullable=False)
+    artist = Column(String, max_length=100, nullable=False)
+    lyrics = Column(String, nullable=False)
+    genius_id = Column(String, nullable=False)
+    
+    user_id = Column(Integer, ForeignKey("users.id"))
+    playlist_id = Column(Integer, ForeignKey("playlist.id"))
 
-#     # category_id = Column(Integer, ForeignKey("categories.id"))
+    user = relationship("User", backref="songs", lazy="joined")
+    category = relationship("Playlist", backref="songs", lazy="joined")
 
-#     # Example relationship definitions
-#     # category = relationship("Category", backref="items", lazy="joined")
+
+class Playlist(Base, TimestampMixin):
+    """Model for an item in the inventory."""
+
+    __tablename__ = "playlists"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+
+class PlaylistSong(Base):
+    """Model for the relationship between a playlist and a song."""
+
+    __tablename__ = "playlist_songs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    
+    playlist_id = Column(Integer, ForeignKey("playlists.id"))
+    spotify_id = Column(Integer, ForeignKey("songs.id))
+
+    playlist = relationship("Playlist", backref="playlist_song", lazy="joined")
